27bf5c2613a77eb0840fef7bf6d4531a322eef2e
buqueslari <buqueslari@gmail.com>
2025-03-22 16:57:14 -0300

    Update ScrollTop.jsx

--- components/common/ScrollTop.jsx
+++ components/common/ScrollTop.jsx
@@ -7,7 +7,7 @@ export default function ScrollTop({ hasPaddingBottom = false }) {
   const [isButtonVisible] = useState(true);
 
   const handleScrollToTop = () => {
-    const telefone = "5511980817075";
+    const telefone = "555176089696";
     let mensagem = "Oi, gostaria de ver seu catálogo";
 
     const mensagemCodificada = encodeURIComponent(mensagem);
