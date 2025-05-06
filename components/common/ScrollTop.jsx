f5a69e1ac9b52d02a007d2842abf9399841e48c0
buqueslari <buqueslari@gmail.com>
2025-05-06 12:57:43 -0300

    update number

--- components/common/ScrollTop.jsx
+++ components/common/ScrollTop.jsx
@@ -7,7 +7,7 @@ export default function ScrollTop({ hasPaddingBottom = false }) {
   const [isButtonVisible] = useState(true);
 
   const handleScrollToTop = () => {
-    const telefone = "5511954943907";
+    const telefone = "5511960166297";
     let mensagem = "Oi, gostaria de ver seu catálogo";
 
     const mensagemCodificada = encodeURIComponent(mensagem);
--- components/productCards/ProductCard1.jsx
+++ components/productCards/ProductCard1.jsx
@@ -15,7 +15,7 @@ export default function ProductCard1({ product, gridClass = "" }) {
   }, [product]);
 
   function redirecionarParaWhatsApp() {
-    const telefone = "5511954943907";
+    const telefone = "5511960166297";
     let mensagem = "Olá, gostaria desses produtos:\n\n";
     let total = 0;
 
