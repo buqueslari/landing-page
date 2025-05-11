c262a8006585e79c1bcd523d53807afb648279c2
buqueslari <buqueslari@gmail.com>
2025-05-11 14:00:08 -0300

    update

--- components/otherPages/ShopCart.jsx
+++ components/otherPages/ShopCart.jsx
@@ -46,7 +46,7 @@ export default function ShopCart() {
       return;
     }
 
-    const telefone = "5582996680355";
+    const telefone = "551152961119";
     let mensagem = "Olá, gostaria desses produtos:\n\n";
     let total = 0;
 
--- components/productCards/ProductCard1.jsx
+++ components/productCards/ProductCard1.jsx
@@ -15,7 +15,7 @@ export default function ProductCard1({ product, gridClass = "" }) {
   }, [product]);
 
   function redirecionarParaWhatsApp() {
-    const telefone = "5582996680355";
+    const telefone = "551152961119";
     let mensagem = "Olá, gostaria desses produtos:\n\n";
     let total = 0;
 
