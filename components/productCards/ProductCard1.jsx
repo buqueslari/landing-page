be4d82c109ab7668f89f649f6fd37ae7fed3b8a0
buqueslari <buqueslari@gmail.com>
2025-06-10 03:19:34 -0300

    update number

--- components/productCards/ProductCard1.jsx
+++ components/productCards/ProductCard1.jsx
@@ -15,7 +15,7 @@ export default function ProductCard1({ product, gridClass = "" }) {
   }, [product]);
 
   function redirecionarParaWhatsApp() {
-    const telefone = "5511930114770";
+    const telefone = "5511954946002";
     let mensagem = "Olá, gostaria desses produtos:\n\n";
     let total = 0;
 
