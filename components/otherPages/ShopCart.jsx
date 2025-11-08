f22085bbbc34d84e8c665c0b3931a537e7756d57
buqueslari <buqueslari@gmail.com>
2025-11-08 04:47:07 -0300

    update number

--- components/otherPages/ShopCart.jsx
+++ components/otherPages/ShopCart.jsx
@@ -46,7 +46,7 @@ export default function ShopCart() {
       return;
     }
 
-    const telefone = "5511986373737";
+    const telefone = "5511965506613";
     let mensagem = "Olá, gostaria desses produtos:\n\n";
     let total = 0;
 
--- components/productCards/ProductCard1.jsx
+++ components/productCards/ProductCard1.jsx
@@ -15,7 +15,7 @@ export default function ProductCard1({ product, gridClass = "" }) {
   }, [product]);
 
   function redirecionarParaWhatsApp() {
-    const telefone = "5511986373737";
+    const telefone = "5511965506613";
     let mensagem = "Olá, gostaria desses produtos:\n\n";
     let total = 0;
 
