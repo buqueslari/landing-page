c67c59f7a21556c8dbe57e98e0c5c940c4f5541e
buqueslari <buqueslari@gmail.com>
2026-01-04 13:46:12 -0300

    update number

--- components/productDetails/details/Details1.jsx
+++ components/productDetails/details/Details1.jsx
@@ -11,7 +11,7 @@ export default function Details1({ product }) {
   const { addProductToCart, isAddedToCartProducts, addToWishlist, isAddedtoWishlist, isAddedtoCompareItem, addToCompareItem, cartProducts, updateQuantity } = useContextElement();
 
   function redirecionarParaWhatsApp() {
-    const telefone = "5511958986326";
+    const telefone = "551151521939";
     let mensagem = "Olá, gostaria desses produtos:\n\n";
     let total = 0;
 
